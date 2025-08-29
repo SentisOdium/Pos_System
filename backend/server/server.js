f3ab39a9c08588fb500e8781486f4ba27864e222
Sentis Odium <jomaracecarmona@gmail.com>
2025-08-29 23:20:37 +0800

    simple test use of express node server

--- backend/server/server.js
+++ backend/server/server.js
@@ -1,1 +1,22 @@
+const express = require('express');
+const app = express();
+const port = 5000;
 
+app.use(express.json());
+
+app.post('/hello/:id', (req, res) =>{
+    const {id} = req.params;
+    const {logo} = req.body;
+
+    if(!logo){
+        res.status(418).send({message: 'No logo found'})
+    }
+
+    res.send({
+        tshirt: `Logos whit ${logo} and ID of ${id}` 
+    });
+});
+
+app.listen(port, () => {
+    console.log(`Example App Listening on Port ${port}`)
+});
