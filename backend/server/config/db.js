c52d028ed96486d9592c2bbae49829bb03875b83
Sentis Odium <jomaracecarmona@gmail.com>
2025-08-31 22:19:53 +0800

    more exploration on Express js

--- backend/server/config/db.js
+++ backend/server/config/db.js
@@ -1,24 +1,22 @@
-const mysql = require('mysql2/promise');
-const dotenv = require('dotenv');
+import mysql from 'mysql2/promise';
+import dotenv from 'dotenv';
+
 
 dotenv.config();
 
-const connection = mysql.createPool({
-    host: process.env.MYSQL_HOST,
-    user: process.env.MYSQL_USER,
-    password: process.env.MYSQL_PASSWORD,
-    database: process.env.MYSQL_DATABASE,
-    connectionLimit: 25
-});
+    const pool = mysql.createPool({
+        host: process.env.MYSQL_HOST,
+        user: process.env.MYSQL_USER,
+        password: process.env.MYSQL_PASSWORD,
+        database: process.env.MYSQL_DATABASE,
+        connectionLimit: 25
+    });
 
-async function getAccount() {
-    const [rows] = await connection.query("SELECT * FROM accounts");
-    return rows;
-}
+    async function getAccounts(){
+            const [rows] = await pool.query("SELECT * FROM accounts");
+            return rows;      
+    }
+ const accounts = await getAccounts();
+ console.log(accounts);
 
-(async () => {
-    const accounts = await getAccount();
-    console.log(accounts);
-})();
-
-module.exports = connection;
+ export {pool};
