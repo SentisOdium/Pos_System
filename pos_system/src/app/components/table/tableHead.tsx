8bbc6103e551c4e27bb6a30c2b8473f162015587
Sentis Odium <jomaracecarmona@gmail.com>
2025-08-31 17:04:11 +0800

    more exploration in Expreess and NextJs

---
+++ pos_system/src/app/components/table/tableHead.tsx
@@ -0,0 +1,14 @@
+const headers = [
+    {id: 1, KEY: "name",    LABEL: "Name"   },
+    {id: 2, KEY: "email",   LABEL: "Email"  },
+    {id: 3, KEY: "year",    LABEL: "Year"   },
+    {id: 4, KEY: "section", LABEL: "Section"},
+]
+
+export default function TableHead(){
+    return(
+        <div>
+            
+        </div>
+    )
+}
