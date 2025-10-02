03e9c86c2228b136b3ccd0c5d31a6d6f0ade678f
Sentis Odium <jomaracecarmona@gmail.com>
2025-10-02 23:58:17 +0800

    more comments

--- backend/server/database/loginQuery/getAccountByEmail.js
+++ backend/server/database/loginQuery/getAccountByEmail.js
@@ -1,5 +1,6 @@
 import { pool } from "../../config/db.js";
 
+//gets the accound using email
 export async function getAccountByEmail(email){
     try{
         const [rows] = await pool.query("SELECT * FROM accounts WHERE email  =  ? LIMIT 1 ;",[email] )
@@ -10,6 +11,7 @@ export async function getAccountByEmail(email){
     }
 }
 
+///gets the user account throuh id
 export async function getSingleAccount(id) {
   try {
     const [rows] = await pool.query(
@@ -18,6 +20,7 @@ export async function getSingleAccount(id) {
     );
 
     // Return the first row or null if no user found
+    // returns empty array if no user, if there is [{user data  here}]
     return rows.length > 0 ? rows[0] : null;
 
   } catch (err) {
